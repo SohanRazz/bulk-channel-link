02fec4462ddb28e46a7025aa3e2c85e7fc63dcb9
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-07 11:12:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/SohanRazz/bulk-channel-link
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /bulk-channel-linkbulk-channel-link
+  git clone $SOURCE_CODE /bulk-channel-link
 fi
 cd /bulk-channel-link
 pip3 install -U -r requirements.txt
